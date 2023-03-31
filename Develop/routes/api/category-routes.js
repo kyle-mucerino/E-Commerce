95641954cfe6d1cb9b88f25a4b0a0c51831f5d8a
kyle-mucerino <kyle.mucerino@gmail.com>
2023-03-31 12:29:40 -0700

    syntax fixes in model files

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Category.findAll({
     include: [Product],
   })
@@ -11,32 +11,38 @@ router.get('/', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Category.findOne({
-    where: { id: req.params.id, },
+    where: {
+      id: req.params.id,
+    },
     include: [Product],
   })
-  .then((category) => res.json(category))
-  .catch((err) => res.status(400).json(err));
+    .then((category) => res.json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   Category.create(req.body)
     .then((category) => res.status(200).json(category))
     .catch((err) => res.status(400).json(err));
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   Category.update(req.body, {
-    where: { id: req.parama.id, },
+    where: {
+      id: req.parama.id,
+    },
   })
     .then((category) => res.status(200).json(category))
     .catch((err) => res.status(400).json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Category.destroy({
-    where: { id: req.params.id, },
+    where: {
+      id: req.params.id,
+    },
   })
     .then((category) => res.status(200).json(category))
     .catch((err) => res.status(400).json(err));
